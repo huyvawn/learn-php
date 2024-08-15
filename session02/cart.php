7d2384178966c4250f948fafab2adca572c17349
huyvawn <63355258+huyvawn@users.noreply.github.com>
2024-08-15 21:25:14 +0700

    shopping cart

--- session02/cart.php
+++ session02/cart.php
@@ -1,6 +1,11 @@
 <?php 
+require_once("functions/db.php");
 session_start();
 $cart = isset($_SESSION["cart"])?$_SESSION["cart"]:[];
+foreach($cart as $key=>$item){
+    // echo"ID=$key -- buy_qty=$item<br/>";
+    
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,6 +15,14 @@ $cart = isset($_SESSION["cart"])?$_SESSION["cart"]:[];
 <?php
 include_once("htmlsess2/nav.php")    ;
 ?>
-
+<?php foreach($cart as $key=>$item) : {
+    $sql= "select * from products where id=$key";
+    $product= findById($sql);
+}?>
+    <div class="row">
+        <h4><?php echo $product["name"]; ?></h4>
+        <p>Quantity: <?php echo $item; ?></p>
+    </div>
+<?php endforeach; ?>
 </body>
 </html>
